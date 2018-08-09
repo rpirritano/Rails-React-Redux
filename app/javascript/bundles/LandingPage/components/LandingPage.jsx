193a6e83f27ebc23f337c8655302b12f840292d8
Robert Pirritano <rpirritano@gmail.com>
2018-08-09 14:38:09 -0700

    added basic layout

--- app/javascript/bundles/LandingPage/components/LandingPage.jsx
+++ app/javascript/bundles/LandingPage/components/LandingPage.jsx
@@ -1,29 +1,34 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import Footer from './Footer'
+import LandingHeader from './LandingHeader'
 
-const LandingPage = ({ name, updateName }) => (
-  <div>
-    <h3>
-      Hello, {name}, this is my Landing Page!
-    </h3>
-    <hr />
-    <form >
-      <label htmlFor="name">
-        Say hello to:
-      </label>
-      <input
-        id="name"
-        type="text"
-        value={name}
-        onChange={(e) => updateName(e.target.value)}
-      />
-    </form>
-  </div>
-);
-
-LandingPage.propTypes = {
-  name: PropTypes.string.isRequired,
-  updateName: PropTypes.func.isRequired,
-};
-
-export default LandingPage;
+export default class Landing extends React.Component {
+  render() {
+    return (
+      <div className="landing-container">
+        <div className="landing-dashboard">
+          <LandingHeader />
+          <div className="dashboard-container">
+            <div className="dashboard-title">
+              <h1>Server-Side - React/Rails.</h1>
+            </div>
+            <div className="dashboard-text">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <p>In vulputate diam sed magna posuere, sit amet dapibus lacus sollicitudin. </p>
+              <p>Nulla rhoncus diam at mauris.</p>
+            </div>
+            <div className="dashboard-button-container">
+              <a href="#">
+                <div className="dashboard-button">
+                  <p>GET STARTED</p>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+        <Footer logo={this.props.logo} instagram={this.props.instagram} twitter={this.props.twitter} medium={this.props.medium} />
+      </div>
+    )
+  }
+}
